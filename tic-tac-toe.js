ca2c181e08da2a5159374a271482f6adf14f4f40
root <root@DESKTOP-LE7491G.localdomain>
2020-08-07 12:37:01 -0700

    Added New Game functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,43 +1,56 @@
 let currentPlayerSymbol = "x";
 let squareValues = ["","","","","","","","",""];
 let winner = '';
+const winnerEl = document.getElementById("game-status");
+const newGameButton = document.getElementById("new-game")
 
 const checkGameStatus = () => {
-    const winnerEl = document.getElementById("game-status");
     for(let i = 0; i < squareValues.length; i += 3) {
-        if(squareValues[i] === (squareValues[i+1] && squareValues[i+2]) && squareValues[i]) {
+        if((squareValues[i] === squareValues[i+1]) && (squareValues[i] === squareValues[i+2]) && squareValues[i]) {
             winner = squareValues[i].toUpperCase();
-            winnerEl.innerHTML = `WINNER: ${winner}`;
+            endRound()
             return;
         }
     }
 
     for (let i = 0; i < (squareValues.length-6); i++) {
-        if(squareValues[i] === (squareValues[i+3] && squareValues[i+6])) {
+        if((squareValues[i] === squareValues[i+3]) && (squareValues[i] === squareValues[i+6]) && squareValues[i]) {
             winner = squareValues[i].toUpperCase();
-            winnerEl.innerHTML = `WINNER: ${winner}`;
+            endRound()
             return;
         }
     }
 
-    if((squareValues[0] === (squareValues[4] && squareValues[8])) || 
-        (squareValues[2] === (squareValues[4] && squareValues[6]))) {
+    if(((squareValues[0] === squareValues[4]) && (squareValues[0] === squareValues[8])) || 
+        ((squareValues[2] === squareValues[4]) && (squareValues[2] === squareValues[6]) && squareValues[4])) {
         winner = squareValues[4].toUpperCase();
-        winnerEl.innerHTML = `WINNER: ${winner}`;
+        endRound()
         return;
     }
 
     if(winner === '' && !squareValues.includes('')) {
         winner = 'None';
-        winnerEl.innerHTML = `WINNER: ${winner}`;
+        endRound();
         return;
     }
+    
+    function endRound() {
+        if (winner !== "") {
+            newGameButton.disabled = false;
+        }
+        winnerEl.innerHTML = `WINNER: ${winner}`;
+    }
+
 }
 
 window.addEventListener("DOMContentLoaded", event => {
+    
     const gameBoard = document.getElementById("tic-tac-toe-board");
-
     gameBoard.addEventListener("click", event => {
+        if (winner !== '') {
+            return; 
+        } 
+        
         let id = event.target.id;
         console.log(id);
         if (id.includes("square-")) {
@@ -53,8 +66,20 @@ window.addEventListener("DOMContentLoaded", event => {
         }   
 
         checkGameStatus();
-
+    
     })
+  newGameButton.addEventListener("click", event => {
+        currentPlayerSymbol = "x";
+        squareValues = ["","","","","","","","",""];
+        winner = '';
+        winnerEl.innerHTML = "";
+        for (i = 0; i < 9; i++) {
+            let imgEl = document.getElementById(`square-${i}`)
+            imgEl.innerHTML = "";
+        }
+        newGameButton.disabled = true;
 
+
+  })  
 
 })
