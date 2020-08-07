1c761ee96b1c89f211f62bcd218cc79ccdcef623
Andrew Choi <adc1225.dev@gmail.com>
2020-08-07 14:53:47 -0600

    finished save state

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,7 +3,28 @@ let squareValues = ["","","","","","","","",""];
 let winner = '';
 const winnerEl = document.getElementById("game-status");
 const newGameButton = document.getElementById("new-game")
+const giveUpButton = document.getElementById("give-up");
 
+
+// ====================================================================
+// endRound function - Announces winner and enables new game button
+// ====================================================================
+function endRound() {
+    if (winner !== "") {
+        newGameButton.disabled = false;
+        localStorage.setItem("winner", winner);
+        winnerEl.innerHTML = `WINNER: ${winner}`;
+    }
+    if (winner === "None") {
+        giveUpButton.disabled = true;
+    }
+    
+}
+
+
+// ====================================================================
+// checkGameStatus
+// ====================================================================
 const checkGameStatus = () => {
     for(let i = 0; i < squareValues.length; i += 3) {
         if((squareValues[i] === squareValues[i+1]) && (squareValues[i] === squareValues[i+2]) && squareValues[i]) {
@@ -34,18 +55,52 @@ const checkGameStatus = () => {
         return;
     }
     
-    function endRound() {
-        if (winner !== "") {
-            newGameButton.disabled = false;
-        }
-        winnerEl.innerHTML = `WINNER: ${winner}`;
-    }
 
 }
 
+
+// ====================================================================
+// addEventListener - DOMContentLoader
+// ====================================================================
 window.addEventListener("DOMContentLoaded", event => {
     
     const gameBoard = document.getElementById("tic-tac-toe-board");
+    
+
+    // ====================================================================
+    // onReload function
+    // ====================================================================
+        
+        function onReload () {
+            if("savedValues" in localStorage) {
+                squareValues = JSON.parse(localStorage.getItem("savedValues"));
+                currentPlayerSymbol = localStorage.getItem("next-turn");
+
+                if("winner" in localStorage) {
+                    winner = localStorage.getItem("winner");
+                    endRound();
+                } 
+            
+                // console.log(typeof squareValues);
+
+                for(let i = 0; i < squareValues.length; i++) {
+                    if(squareValues[i] === "x") {
+                        let selectedSquare = document.getElementById(`square-${i}`);
+                        selectedSquare.innerHTML = 
+                        `<img src="https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg">`
+                    } else if (squareValues[i] === "o") {
+                        let selectedSquare = document.getElementById(`square-${i}`);
+                        selectedSquare.innerHTML =
+                            `<img src="https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg">`
+                    }
+                }
+            }
+
+        }
+    
+        onReload();
+   
+
     gameBoard.addEventListener("click", event => {
         if (winner !== '') {
             return; 
@@ -57,7 +112,8 @@ window.addEventListener("DOMContentLoaded", event => {
             let gridIndex = parseInt(id[id.length -1])
             if (squareValues[gridIndex] === "") {
                     let selectedSquare = document.getElementById(id);
-                    selectedSquare.innerHTML = `<img src="https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-${currentPlayerSymbol}.svg"> </img>`
+                    selectedSquare.innerHTML = 
+                    `<img src="https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-${currentPlayerSymbol}.svg">`
                     squareValues[gridIndex] = currentPlayerSymbol;
                 if (currentPlayerSymbol === "o") {    
                     currentPlayerSymbol = "x";
@@ -65,9 +121,21 @@ window.addEventListener("DOMContentLoaded", event => {
             }
         }   
 
+
+        localStorage.setItem("savedValues", JSON.stringify(squareValues));
+        localStorage.setItem("next-turn", currentPlayerSymbol);
+        // console.log('local storage ', localStorage);
+        // console.log(JSON.parse(localStorage.getItem("savedValues")));
+
+        giveUpButton.disabled = false;
         checkGameStatus();
     
     })
+
+
+// ====================================================================
+// New game Button
+// ====================================================================
   newGameButton.addEventListener("click", event => {
         currentPlayerSymbol = "x";
         squareValues = ["","","","","","","","",""];
@@ -79,7 +147,24 @@ window.addEventListener("DOMContentLoaded", event => {
         }
         newGameButton.disabled = true;
 
+        localStorage.removeItem("savedValues");
+        localStorage.removeItem("next-turn");
+        localStorage.removeItem("winner");
+    })  
 
-  })  
+// ====================================================================
+// Give up button
+// ====================================================================
+  giveUpButton.addEventListener("click", e => {
+    if(currentPlayerSymbol === "x") {
+        winner = "O";
+        endRound();
+    } else {
+        winner = "X";
+        endRound();
+    }
 
+    giveUpButton.disabled = true;
+    newGameButton.disabled = false;
+  });
 })
