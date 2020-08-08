428dc9a97918728d783dfb6f42d6820fbf074e11
root <root@DESKTOP-LE7491G.localdomain>
2020-08-08 05:58:38 -0700

    Debugged entire app, AI now works by randomly selecting an index containing an empty string in squareValues and manipulating the corresponding element in the HTML

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,11 +2,10 @@ let playerSymbol = '';
 let computerSymbol = '';
 let computerTurn;
 let squareValues = ["","","","","","","","",""];
-let currentPlayerSymbol = ''
 let winner = '';
-let xScore = 0;
+let playerScore = 0;
 let drawCount = 0;
-let oScore = 0;
+let computerScore = 0;
 const scoreCountEl = document.getElementById("score-count")
 const winnerEl = document.getElementById("game-status");
 const newGameButton = document.getElementById("new-game");
@@ -15,12 +14,16 @@ const resetButton = document.getElementById("score-count-button")
 const turnEl = document.getElementById("turn");
 
 function updateScoreCount() {
-    let scoreCount = `"X" has won ${xScore} games and "O" has won ${oScore} games with ${drawCount} draws`
+    let scoreCount = `The computer has won ${computerScore} ${(computerScore === 1) ? "game" : "games"} and the player has won ${playerScore} ${(playerScore === 1) ? "game" : "games"} There ${(drawCount === 1 ? "has" : "have")} been ${drawCount} ${(drawCount === 1 ? "draw" : "draws")}`
     scoreCountEl.innerHTML = `${scoreCount}`
 }
 
 function whosTurn() {
-    turnEl.innerHTML = `It is currently "${currentPlayerSymbol.toUpperCase()}" 's turn.`
+    if (winner === "") {
+    turnEl.innerHTML = `It is currently ${(computerTurn) ? "the Computer's" : "the Player's"} turn. ("${(computerTurn) ? computerSymbol.toUpperCase() : playerSymbol.toUpperCase()}")`
+    } else if (winner !== "") {
+        turnEl.innerHTML = "Hit the New Game Button to start a new round"
+    }
 }
 
 // ====================================================================
@@ -29,16 +32,15 @@ function whosTurn() {
 function endRound() {
     if (winner !== "") {
         newGameButton.disabled = false;
-        // localStorage.setItem("winner", winner);
-        (winner === "X") ? xScore++ : (winner === "O") ? oScore++ : drawCount++
-        // localStorage.setItem("xScore",xScore);
-        // localStorage.setItem("oScore",oScore);
-        // localStorage.setItem("drawCount",drawCount);
+        giveUpButton.disabled = true;
+        localStorage.setItem("winner", winner);
+        (winner === playerSymbol.toUpperCase()) ? playerScore++ : (winner === computerSymbol.toUpperCase()) ? computerScore++ : drawCount++
+        localStorage.setItem("playerScore", playerScore);
+        localStorage.setItem("computerScore",computerScore);
+        localStorage.setItem("drawCount",drawCount);
         winnerEl.innerHTML = `WINNER: ${winner}`;
         updateScoreCount();
-    }
-    if (winner === "None") {
-        giveUpButton.disabled = true;
+        whosTurn();
     }
     
 }
@@ -64,7 +66,7 @@ const checkGameStatus = () => {
         }
     }
     
-    if(((squareValues[0] === squareValues[4]) && (squareValues[0] === squareValues[8])) || 
+    if(((squareValues[0] === squareValues[4]) && (squareValues[0] === squareValues[8])) && squareValues[4] || 
     ((squareValues[2] === squareValues[4]) && (squareValues[2] === squareValues[6]) && squareValues[4])) {
         winner = squareValues[4].toUpperCase();
         endRound()
@@ -78,7 +80,6 @@ const checkGameStatus = () => {
     }
     if(computerTurn === true) {
         computerMove();
-        // setTimeout(computerMove, 1000);
     }
     
 }
@@ -92,37 +93,45 @@ function assignSymbol() {
         computerSymbol = "x";
         playerSymbol = "o";
         computerTurn = true;
-        // localStorage.setItem("isComputerTurn",computerTurn)
+        localStorage.setItem("isComputerTurn",computerTurn);
+        localStorage.setItem("playerSymbol",playerSymbol);
+        localStorage.setItem("computerSymbol",computerSymbol);
         computerMove();
     } else {
 ;        // player starts first
         computerSymbol = "o"
         playerSymbol = "x";
         computerTurn = false;
-        // localStorage.setItem("isComputerTurn",computerTurn)
+        localStorage.setItem("playerSymbol",playerSymbol);
+        localStorage.setItem("computerSymbol",computerSymbol);
+        localStorage.setItem("isComputerTurn",computerTurn)
     }
+    whosTurn();
     
 }
 
 function computerMove() {
     let randomGridBlock = Math.floor(Math.random() * 9);
-
+    
     while(squareValues[randomGridBlock] !== '') {
         randomGridBlock = Math.floor(Math.random() * 9);
     }
-
+    if (computerTurn) {
+    setTimeout(function(){
+        if (squareValues[randomGridBlock] === "") {
         const selectedSquare = document.getElementById(`square-${randomGridBlock}`);
         selectedSquare.innerHTML =
-            `<img src="https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-${computerSymbol}.svg">`;
+        `<img src="https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-${computerSymbol}.svg">`;
         
         squareValues[randomGridBlock] = computerSymbol;
-
-        console.log(squareValues);
         computerTurn = false;
-        // localStorage.setItem("isComputerTurn",computerTurn)
+        localStorage.setItem("isComputerTurn",computerTurn)
         checkGameStatus();
-        currentPlayerSymbol =  (computerTurn) ? computerSymbol : playerSymbol
-        // localStorage.setItem("next-turn", currentPlayerSymbol);
+        localStorage.setItem("savedValues", JSON.stringify(squareValues));
+        whosTurn();
+        }
+    }, 3000 )
+    }
     }
 
     
@@ -132,131 +141,137 @@ function computerMove() {
     window.addEventListener("DOMContentLoaded", event => {
         const gameBoard = document.getElementById("tic-tac-toe-board");
         
-        
         // ====================================================================
         // onReload function
         // ====================================================================
         
-    //     function onReload () {
+        function onReload () {
             
-    //         whosTurn();
-    //         if("savedValues" in localStorage) {
-    //             squareValues = JSON.parse(localStorage.getItem("savedValues"));
-    //             currentPlayerSymbol = localStorage.getItem("next-turn");
-                
-    //             if("winner" in localStorage) {
-    //                 winner = localStorage.getItem("winner");
-    //                 if (winner !== "") {
-    //                     newGameButton.disabled = false;
-    //                     winnerEl.innerHTML = `WINNER: ${winner}`;
-    //                 }
-    //             if ("isComputerTurn" in localStorage) {
-    //                 computerTurn = JSON.parse(localStorage.getItem("isComputerTurN"))
-    //             }            
-    //             } 
-                
-    //             // console.log(typeof squareValues);
+            if ( "playerScore" in localStorage) {
+                playerScore = parseInt(localStorage.getItem( "playerScore"));
+                computerScore = parseInt(localStorage.getItem("computerScore"));
+                drawCount = parseInt(localStorage.getItem("drawCount"));
+                updateScoreCount();
+            }
             
-    //         for(let i = 0; i < squareValues.length; i++) {
-    //             if(squareValues[i] === "x") {
-    //                 let selectedSquare = document.getElementById(`square-${i}`);
-    //                 selectedSquare.innerHTML = 
-    //                 `<img src="https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg">`
-    //             } else if (squareValues[i] === "o") {
-    //                 let selectedSquare = document.getElementById(`square-${i}`);
-    //                 selectedSquare.innerHTML =
-    //                 `<img src="https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg">`
-    //             }
-    //         }
-    //     }
-    //     if ("xScore" in localStorage) {
-    //         xScore = parseInt(localStorage.getItem("xScore"));
-    //         oScore = parseInt(localStorage.getItem("oScore"));
-    //         drawCount = parseInt(localStorage.getItem("drawCount"));
-    //     }
-    //     updateScoreCount();
-    // }
-    
-    // onReload();
-    
+            if("savedValues" in localStorage) {
+                squareValues = JSON.parse(localStorage.getItem("savedValues"));
+                console.log(squareValues);
+            }
+            
+            if("winner" in localStorage) {
+                winner = localStorage.getItem("winner");
+                    if (winner !== "") {
+                        newGameButton.disabled = false;
+                        winnerEl.innerHTML = `WINNER: ${winner}`;
+                    }
+                }            
+                if ("isComputerTurn" in localStorage && squareValues.includes("")) {
+                    computerTurn = JSON.parse(localStorage.getItem("isComputerTurn"))
+                    playerSymbol = localStorage.getItem("playerSymbol");
+                    computerSymbol = localStorage.getItem("computerSymbol")
+                    whosTurn();
+                } 
+                
+            }
+            
+            onReload();
+  
+            for(let i = 0; i < squareValues.length; i++) {
+                if(squareValues[i] === "x") {
+                    let selectedSquare = document.getElementById(`square-${i}`);
+                selectedSquare.innerHTML = 
+                `<img src="https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg">`
+            } else if (squareValues[i] === "o") {
+                let selectedSquare = document.getElementById(`square-${i}`);
+                selectedSquare.innerHTML =
+                `<img src="https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg">`
+            }
+        }
+        function computerFirstMoveAfterReloaad() {
+        if (computerTurn === true && winner === "") {
+            computerMove();
+        } else if (winner !== "") {
+            whosTurn();
+        }
+    }
+
+    computerFirstMoveAfterReloaad();
     
     gameBoard.addEventListener("click", event => {
+
+if (winner !== '' || computerTurn) {
+    return; 
+} 
+
+let id = event.target.id; // id of "square-0"
+if (id.includes("square-")) {
+    let gridIndex = parseInt(id[id.length -1])
+    if (squareValues[gridIndex] === "") {
+        let selectedSquare = document.getElementById(id);
+        selectedSquare.innerHTML = 
+        `<img src="https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-${playerSymbol}.svg">`
+        squareValues[gridIndex] = playerSymbol;
+         }
+        } else return;  
         
-        if (winner !== '' || computerTurn) {
-            return; 
-        } 
+        localStorage.setItem("savedValues", JSON.stringify(squareValues));
         
-        let id = event.target.id; // id of "square-0"
-        if (id.includes("square-")) {
-            let gridIndex = parseInt(id[id.length -1])
-            if (squareValues[gridIndex] === "") {
-                let selectedSquare = document.getElementById(id);
-                selectedSquare.innerHTML = 
-                `<img src="https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-${playerSymbol}.svg">`
-                squareValues[gridIndex] = playerSymbol;
-                 }
-                }   
-                
-                // localStorage.setItem("savedValues", JSON.stringify(squareValues));
-                // localStorage.setItem("next-turn", currentPlayerSymbol);
-                // // console.log('local storage ', localStorage);
-                // console.log(JSON.parse(localStorage.getItem("savedValues")));
-                
-                giveUpButton.disabled = false;
-                computerTurn = true;
-                // localStorage.setItem("isComputerTurn",computerTurn)
-                currentPlayerSymbol =  (computerTurn) ? computerSymbol : playerSymbol
-                checkGameStatus();
-                whosTurn();
-        
-    })
-    
-    
-    // ====================================================================
-    // New game Button
-    // ====================================================================
-    newGameButton.addEventListener("click", event => {
-        currentPlayerSymbol = "";
-        squareValues = ["","","","","","","","",""];
-        winner = '';
-        winnerEl.innerHTML = "";
-        for (i = 0; i < 9; i++) {
-            let imgEl = document.getElementById(`square-${i}`)
-            imgEl.innerHTML = "";
-        }
-        newGameButton.disabled = true;
-        
-        // localStorage.removeItem("savedValues");
-        // localStorage.removeItem("next-turn");
-        // localStorage.removeItem("isComputerTurn")
-        // localStorage.removeItem("winner");
-        assignSymbol();
-    })  
-    
-    // ====================================================================
-    // Give up button
-    // ====================================================================
-    giveUpButton.addEventListener("click", e => {
-        if(currentPlayerSymbol === "x") {
-            winner = "O";
-            endRound();
-        } else {
-            winner = "X";
-            endRound();
-        }
-        
-        giveUpButton.disabled = true;
-        newGameButton.disabled = false;
-    });
-    
-    // resetButton.addEventListener("click", event => {
-    //     localStorage.removeItem("xScore");
-    //     localStorage.removeItem("oScore");
-    //     localStorage.removeItem("drawCount");
-    //     xScore = 0;
-    //     drawCount = 0;
-    //     oScore = 0;
-    //     updateScoreCount();
-    // })
-    assignSymbol();
+        giveUpButton.disabled = false;
+        computerTurn = true;
+        localStorage.setItem("isComputerTurn",computerTurn)
+        whosTurn();
+        checkGameStatus();
+
+})
+
+
+// ====================================================================
+// New game Button
+// ====================================================================
+newGameButton.addEventListener("click", event => {
+localStorage.removeItem("savedValues");
+localStorage.removeItem("next-turn");
+localStorage.removeItem("isComputerTurn")
+localStorage.removeItem("winner");
+squareValues = ["","","","","","","","",""];
+winner = '';
+winnerEl.innerHTML = "";
+for (i = 0; i < 9; i++) {
+    let imgEl = document.getElementById(`square-${i}`)
+    imgEl.innerHTML = "";
+}
+updateScoreCount();
+newGameButton.disabled = true;
+
+assignSymbol();
+})  
+
+// ====================================================================
+// Give up button
+// ====================================================================
+giveUpButton.addEventListener("click", e => {
+if(playerSymbol === "x") {
+    winner = "O";
+    endRound();
+} else {
+    winner = "X";
+    endRound();
+}
+
+giveUpButton.disabled = true;
+newGameButton.disabled = false;
+});
+
+resetButton.addEventListener("click", event => {
+localStorage.removeItem( "playerScore");
+localStorage.removeItem("computerScore");
+localStorage.removeItem("drawCount");
+playerScore = 0;
+drawCount = 0;
+computerScore = 0;
+updateScoreCount();
+})
+if (localStorage.getItem("playerSymbol") === null) {
+assignSymbol(); }
 })
